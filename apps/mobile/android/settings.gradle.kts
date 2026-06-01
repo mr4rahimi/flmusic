d411dcf82d78c3e9eea2aa4965a59dede602ba51
Mehdi Rahimi <mr4.rahimi@gmail.com>
2026-06-01 15:00:52 +0330

    fix: add correct settings.gradle.kts for android build

--- apps/mobile/android/settings.gradle.kts
+++ apps/mobile/android/settings.gradle.kts
@@ -6,23 +6,18 @@ pluginManagement {
         require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
         flutterSdkPath
     }
-
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
     repositories {
-        maven { url = uri("https://maven.myket.ir") }
-        maven { url = uri("https://plugins.gradle.org/m2/") }
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
-
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
@@ -31,5 +26,4 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
 include(":app")
